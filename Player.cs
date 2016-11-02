0b5e2ae305afb0b7f8900e75dc6e71fa36a67b81
minhhao97vn <minhhao97vn@gmail.com>
2016-11-02 22:10:53 +0700

    Update xây được nhiều loại tower

--- Player.cs
+++ Player.cs
@@ -14,15 +14,18 @@ namespace TowerDefenseOOP
         #region Khai báo
 
         int[,] map = new int[Container.MapHeight,Container.MapWidth];    //Bản đồ trò chơi
-        List<Vector2> roadCenters = new List<Vector2>();//Danh sách các vector2 chứa các center của các ô có đường đi
-        List<Vector2> rockCenters = new List<Vector2>();//Danh sách các vector2 chứa các center của các ô có đá/rừng
+        //List<Vector2> roadCenters = new List<Vector2>();//Danh sách các vector2 chứa các center của các ô có đường đi
+        //List<Vector2> rockCenters = new List<Vector2>();//Danh sách các vector2 chứa các center của các ô có đá/rừng
         List<Texture2D> bulletTextureList = new List<Texture2D>();
         List<Texture2D> towerTextureList = new List<Texture2D>();
         List<Texture2D> enemyTextureList = new List<Texture2D>();
+        List<MachineGunTower> MachineGunTowerList = new List<MachineGunTower>();
         List<RocketTower> rocketTowerList = new List<RocketTower>();
         List<Enemy> enemyList = new List<Enemy>();
 
+        private Texture2D mouseTexture;     //Texture dùng để check vị trí xây dựng tower có hợp lệ không
         private Texture2D baseTexture;
+        float scale;
         int level;
         public int Level
         {
@@ -32,6 +35,9 @@ namespace TowerDefenseOOP
 
         private Vector2 position;
         private Vector2 origin;
+
+        private MouseState mouseState;
+        private MouseState oldState;
 
         MouseManager mouseManager;
 
@@ -43,11 +49,61 @@ namespace TowerDefenseOOP
             this.map = map.MapList[level - 1];              //Tùy vào level sẽ có map khác nhau
             this.level = level;                             //Level của màn chơi
             origin = new Vector2(Container.towerSize / 2, Container.towerSize / 2);
+            this.scale = Container.enemyTextureScale;
+            mouseState = oldState;
         }
+
+        //Hàm lấy vào tất cả các ô nằm trên đường đi add vào roadCenters và các ô chứa đá add vào rockCenters
+        //public void addPositions()
+        //{
+        //    Vector2 temp = new Vector2(Container.towerSize / 2, Container.towerSize / 2);
+        //    for(int y=0;y<Container.MapHeight;y++)
+        //    {
+        //        for(int x=0;x<Container.MapWidth;x++)
+        //        {
+        //            if(map[y,x]==1)//Chỉ số các ô nằm trên đường đi bằng 1
+        //            {
+        //                Vector2 temp2 = new Vector2(x, y) * Container.tileSize + temp;//Lấy vị trí tâm của ô 
+        //                roadCenters.Add(temp2);  //Thêm vào list
+        //            }
+        //            if (map[y, x] == 2)//Chỉ số các ô chứa đá bằng 2
+        //            {
+        //                Vector2 temp2 = new Vector2(x, y) * Container.tileSize + temp;//Lấy vị trí tâm của ô 
+        //                rockCenters.Add(temp2);  //Thêm vào list
+        //            }
+        //        }
+        //    }
+        //}
+
+        //Hàm kiểm tra xem có thể xây Tower ở vị trí nhập vào không
+
+        //public bool isTowerAvailable(Vector2 position)
+        //{
+        //    int width = Container.MapWidth;
+        //    int height = Container.MapHeight;
+        //    foreach (Vector2 roadCenter in roadCenters)
+        //    {
+        //        if (Vector2.Distance(position, roadCenter) < (float)Container.tileSize*(scale+0.05f))
+        //            return false;
+        //    }
+        //    foreach (Vector2 rockCenter in rockCenters)
+        //    {
+        //        if (Vector2.Distance(position, rockCenter) < (float)Container.tileSize * 2)
+        //            return false;
+        //    }
+        //    foreach (MachineGunTower rk in MachineGunTowerList)
+        //    {
+        //        if (Vector2.Distance(position, rk.Position) < (float)Container.towerSize * (scale + 0.05f))
+        //            return false;
+        //    }
+        //    return true;
+        //}
+
 
         //Hàm LoadContent
         public void LoadContent(ContentManager content)
         {
+            mouseTexture = content.Load<Texture2D>("hover");        //Load mouseTexture
             mouseManager = new MouseManager(map, Level);
             mouseManager.LoadContent(content);
             for (int i = 0; i < Container.numberOfTowers;i++ )
@@ -62,28 +118,43 @@ namespace TowerDefenseOOP
             {
                 Texture2D enemy = content.Load<Texture2D>("enemy_0" + i.ToString("d2"));
                 enemyTextureList.Add(enemy);
-            }                                       
+            }
+            //addPositions();                                         
         }
 
         //Hàm update
-        public void Update(GameTime gameTime,List<Enemy>enemyList)
+        public void Update(GameTime gameTime, List<Enemy> enemyList)
         {
             mouseManager.Update(gameTime);
-            if (MouseManager.buildTower == 1)
+            if (MouseManager.buildTower == 3)
             {
                 RocketTower rT = new RocketTower(towerTextureList[3], 4, MouseManager.position, baseTexture, bulletTextureList[3]);
                 rocketTowerList.Add(rT);
             }
-           
+            else if (MouseManager.buildTower == 0)
+            {
+                MachineGunTower mgT = new MachineGunTower(towerTextureList[0], 4, MouseManager.position, baseTexture, bulletTextureList[0]);
+                MachineGunTowerList.Add(mgT);
+            }
 
             //Update Towers
-            for (int i = 0; i < rocketTowerList.Count;i++ )
+            for (int i = 0; i < rocketTowerList.Count; i++)
             {
                 if (rocketTowerList[i].IsAlive)
-                    rocketTowerList[i].Update(gameTime,enemyList);
+                    rocketTowerList[i].Update(gameTime, enemyList);
                 else
                 {
                     rocketTowerList.RemoveAt(i);
+                    i--;
+                }
+            }
+            for (int i = 0; i < MachineGunTowerList.Count; i++)
+            {
+                if (MachineGunTowerList[i].IsAlive)
+                    MachineGunTowerList[i].Update(gameTime, enemyList);
+                else
+                {
+                    MachineGunTowerList.RemoveAt(i);
                     i--;
                 }
             }
@@ -93,11 +164,19 @@ namespace TowerDefenseOOP
         public void Draw(SpriteBatch spriteBatch)
         {
             mouseManager.Draw(spriteBatch);
-            foreach(RocketTower rk in rocketTowerList)
+            foreach (RocketTower rk in rocketTowerList)
             {
-                if(rk.IsAlive)
+                if (rk.IsAlive)
                 {
                     rk.Draw(spriteBatch);
+                }
+            }
+
+            foreach (MachineGunTower mgT in MachineGunTowerList)
+            {
+                if (mgT.IsAlive)
+                {
+                    mgT.Draw(spriteBatch);
                 }
             }
             //Vẽ enemy
