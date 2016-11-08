73a66eef26840dbc2cf6dbe65ff08cbe307f3dab
hoanglechanhtu <hoanglechanhtu01@gmail.com>
2016-11-08 08:48:25 +0700

    Revert "Revert "<>Tu""
    This reverts commit 4746344400038e811edefe8631567a0255e4c893.

--- towerButton.cs
+++ towerButton.cs
@@ -15,38 +15,38 @@ namespace TowerDefenseOOP
         Texture2D texture;
         Vector2 position;
         Rectangle rectangle;
-
+        Texture2D buttondetail;
         Color colour = new Color(255, 255, 255, 255);
         public Vector2 size;
-        public towerButton(Texture2D newTexture, GraphicsDevice graphics, Vector2 tempSize)
+        public towerButton(Texture2D newTexture,Texture2D buttonDetail, GraphicsDevice graphics, Vector2 tempSize)
         {
             texture = newTexture;
+            buttondetail = buttonDetail;
             //scrW = 900 scrH 600
             //imW      imH
             size = tempSize;
+            
         }
-        bool down;
+        bool hover;
         public bool isClicked;
         public void Update(MouseState mouse,int goldHave)
         {
             rectangle = new Rectangle((int)position.X, (int)position.Y, (int)size.X, (int)size.Y);
             Rectangle mouseRectangle = new Rectangle(mouse.X, mouse.Y, 1, 1);
-            if (goldHave < Price)
-                colour = colour;
-         
-            //if (mouseRectangle.Intersects(rectangle))
-            //{
-            //    if (colour.A == 255) down = false;
-            //    if (colour.A == 0) down = true;
-            //    if (down) colour.A += 3; else colour.A -= 3;
-            //    if (mouse.LeftButton == ButtonState.Pressed) isClicked = true;
+       
 
-            //}
-            //else if (colour.A < 255)
-            //{
-            //    colour.A += 3;
-            //    isClicked = false;
-            //}
+            if (mouseRectangle.Intersects(rectangle))
+            {
+                if (mouse.LeftButton == ButtonState.Pressed) isClicked = true;
+                hover = true;
+               
+            }
+            else
+            {
+                
+                isClicked = false;
+                hover = false;
+            }
         }
         public void SetPosition(Vector2 newPosition)
         {
@@ -55,6 +55,8 @@ namespace TowerDefenseOOP
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(texture, rectangle, colour);
+            if (hover)
+                spriteBatch.Draw(buttondetail,new Rectangle((int)position.X+30,(int)position.Y+30,130,170), new Rectangle(0, 0, 130, 170), Color.White);
         }
         public void setPrice(int price)
         {
